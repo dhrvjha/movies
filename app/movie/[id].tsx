8139be42becf5f4d192822950c53ec852e42b603
Dhruv Kumar Jha <kdhruv71@gmail.com>
2025-06-14 20:58:03 +0530

    first signed commit

---
+++ app/movie/[id].tsx
@@ -0,0 +1,10 @@
+
+
+import { Text, View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+export default function Details () {
+    const { id } = useLocalSearchParams();
+  return (
+    <Text>Details </Text>
+  );
+}
---
+++ app/onboarding.tsx
@@ -0,0 +1,12 @@
+
+import { Text, View } from "react-native";
+
+export default function Onboarding () {
+  return (
+    <View
+        className="flex-1 justify-center items-center"
+    >
+      <Text className="text-5xl text-accent font-bold">OnBoarding.</Text>
+    </View>
+  );
+}
